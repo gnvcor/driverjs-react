8381b206c66c9330ef329dbbe917cb6867e64081
gnvcor <nikita.kolibri@gmail.com>
2024-05-05 17:59:37 +0500

    Added return type for hook useDriver

--- src/hooks/useDriver.tsx
+++ src/hooks/useDriver.tsx
@@ -1,7 +1,8 @@
 import { useContext } from 'react';
 import { DriverContext } from '../providers/DriverProvider';
+import {Driver as DriverType} from 'driver.js'
 
-export const useDriver = () => {
+export const useDriver = (): { driver?: DriverType } => {
     const { driver } = useContext(DriverContext);
 
     return {
