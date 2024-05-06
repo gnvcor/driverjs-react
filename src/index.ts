daa4bffe2ee03c0c300eaaf554fe7388ef777be1
gnvcor <nikita.kolibri@gmail.com>
2024-05-06 09:58:20 +0500

    Removed console.log

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { useDriver } from './hooks/useDriver';
 import { DriverProvider } from './providers/DriverProvider';
 
-console.log('init')
-
 export default {
     useDriver,
     DriverProvider,
