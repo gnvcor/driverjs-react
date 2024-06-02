3e9a07c5060ed0ed05c6fe55eeddfb28854eefb0
Nikita Gordeev <nikita.kolibri@gmail.com>
2024-06-02 21:22:24 +0500

    Fixed example

--- example/DriverJsReactContainer.tsx
+++ example/DriverJsReactContainer.tsx
@@ -1,5 +1,5 @@
-import React, { FC, useEffect } from 'react';
-import { useDriver } from '../src';
+import React, { FC } from 'react';
+import { StepComponent, useDriver } from '../src';
 
 export const DriverJsReactContainer: FC = () => {
   const { driver } = useDriver();
@@ -10,15 +10,24 @@ export const DriverJsReactContainer: FC = () => {
     }
   };
 
-  useEffect(() => {
-    onShowTour();
-  }, [driver]);
+  return (
+      <div>
+          <StepComponent key={0} stepIndex={0} popover={{ title: 'Title Tour Example 1', description: 'Description Tour Example 1' }}>
+              <div>tour-example-1</div>
+          </StepComponent>
 
-  return <div>
-        <div id="tour-example-1">tour-example-1</div>
-        <div id="tour-example-2">tour-example-2</div>
-        <div id="tour-example-3">tour-example-3</div>
-        <div id="tour-example-4">tour-example-4</div>
-        <button onClick={onShowTour}>Show tour</button>
-    </div>;
+          <StepComponent key={1} stepIndex={1} popover={{ title: 'Title Tour Example 2', description: 'Description Tour Example 2' }}>
+              <div>tour-example-2</div>
+          </StepComponent>
+
+          <StepComponent key={2} stepIndex={2} popover={{ title: 'Title Tour Example 3', description: 'Description Tour Example 3' }}>
+              <div>tour-example-3</div>
+          </StepComponent>
+
+          <StepComponent key={3} stepIndex={3} popover={{ title: 'Title Tour Example 4', description: 'Description Tour Example 4' }}>
+              <div>tour-example-4</div>
+          </StepComponent>
+          <button onClick={onShowTour}>Show tour</button>
+      </div>
+  );
 };
